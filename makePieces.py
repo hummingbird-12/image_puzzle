273c36cd2652d75c6f682acc106d02d246eb4525
Hummingbird-12 <goodboyih96@gmail.com>
2019-02-23 20:04:23 +0900

    move piece maker to separate file

---
+++ makePieces.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+import Piece as Pc
+
+
+def get_pieces(img, img_row, img_col, img_chn):
+    # piece information
+    cnt_column = int(input("Pieces in a column: "))
+    cnt_row = int(input("Pieces in a row: "))
+    cnt_total = cnt_row * cnt_column
+    size_vertical = math.ceil(img_col / cnt_column)
+    size_horizontal = math.ceil(img_row / cnt_row)
+    p_list = []
+
+    # creation of pieces
+    for pIt in range(cnt_total):
+        temp = np.zeros((size_vertical, size_horizontal, img_chn), dtype=np.uint8)
+        start_row = size_vertical * math.floor(pIt / cnt_row)
+        start_col = size_horizontal * (pIt % cnt_row)
+        # temp[:] = img[start_row:start_row + pDim, start_col:start_col + pDim]
+        for i in range(start_row, start_row + size_vertical):
+            if i >= img_row:
+                break
+            for j in range(start_col, start_col + size_horizontal):
+                if j >= img_col:
+                    continue
+                temp[i - start_row][j - start_col] = img[i][j]
+        p_list.append(Pc.Piece(pIt, size_vertical, size_horizontal, img_chn, (start_row, start_col), temp))
+    return p_list, size_vertical, size_horizontal, cnt_row, cnt_column, cnt_total
--- puzzler.py
+++ puzzler.py
@@ -1,9 +1,7 @@
 import cv2
-import numpy as np
-import math
 from random import shuffle
 import readImage as readImg
-import Piece as Pc
+import makePieces as makeP
 import drawPieces as drawP
 
 
@@ -11,28 +9,8 @@ import drawPieces as drawP
 filename = "lenna.png"
 img, imgRow, imgCol, imgChn = readImg.read_image(filename, cv2.IMREAD_COLOR)
 
-# piece information
-pCnt_column = int(input("Pieces in a column: "))
-pCnt_row = int(input("Pieces in a row: "))
-pCnt_total = pCnt_row * pCnt_column
-pSize_vertical = math.ceil(imgCol / pCnt_column)
-pSize_horizontal = math.ceil(imgRow / pCnt_row)
-pList = []
-
-# creation of pieces
-for pIt in range(pCnt_total):
-    temp = np.zeros((pSize_vertical, pSize_horizontal, imgChn), dtype=np.uint8)
-    startRow = pSize_vertical * math.floor(pIt / pCnt_row)
-    startCol = pSize_horizontal * (pIt % pCnt_row)
-    # temp[:] = img[startRow:startRow + pDim, startCol:startCol + pDim]
-    for i in range(startRow, startRow + pSize_vertical):
-        if i >= imgRow:
-            break
-        for j in range(startCol, startCol + pSize_horizontal):
-            if j >= imgCol:
-                continue
-            temp[i - startRow][j - startCol] = img[i][j]
-    pList.append(Pc.Piece(pIt, pSize_vertical, pSize_horizontal, imgChn, (startRow, startCol), temp))
+pList, pSize_vertical, pSize_horizontal, pCnt_row, pCnt_column, pCnt_total =\
+    makeP.get_pieces(img, imgRow, imgCol, imgChn)
 
 shuffle(pList)  # randomize pieces
 
